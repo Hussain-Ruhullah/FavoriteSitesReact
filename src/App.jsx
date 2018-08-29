dcc83eaf8fe6c31a556c7a5f185b409f1f8e62fe
Hussain Ruhollah <hussain.ruhollah@gmail.com>
2018-08-29 16:45:55 +0200

    added ssl

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import Intro from './components/Intro';
 import Content from './components/Content';
+import ToLearn from './components/learn';
 
 
 /**
@@ -11,7 +12,10 @@ const App = () => (
     <div>
         <Intro/>
         <Content/>
+        <ToLearn />
     </div>
+
+    
 );
 
 export default hot(module)(App);
---
+++ src/components/learn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class ToLearn extends React.Component {
+
+
+    render(){
+        return(
+            <h1>It's Working!</h1>
+        );
+    }
+}
