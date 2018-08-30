b20f66bc84dd4968390a3e0e158374a1fa79e262
Hussain Ruhollah <hussain.ruhollah@gmail.com>
2018-08-30 17:05:32 +0200

    Form and List Comp
    Form and List Comp

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-
-import { Mode } from 'chayns-components/lib';
-import UserList from './user/UserList';
-import PersonFinder from './personFinder/PersonFinderWrapper';
+import Form from './register/form';
+import SiteList from './directory/listcontainer'
 
 export default class Content extends React.Component {
     constructor(props) {
@@ -16,51 +14,13 @@ export default class Content extends React.Component {
         };
     }
 
-    /**
-     * Push´s an user object to the state 'users' and refresh's the react component 'UserList'
-     * By Calling setState the renderfunction ist called automatically
-     */
-    addUser = (user) => {
-        const { users } = this.state;
-        if (users.find(u => u.userId === user.userId)) {
-            return;
-        }
-
-        this.setState({
-            users: [...users, user]
-        });
-    };
-
-    /**
-     *  locally save the state `users` and filter the array by a userId
-     *  By Calling setState the renderfunction ist called automatically
-     */
-    removeUser = (userId) => {
-        const { users } = this.state;
-        this.setState({
-            users: users.filter(u => u.userId !== userId)
-        });
-    };
-
+   
     render() {
-        const { users } = this.state;
         return (
             <div className="tapp__content content">
-                {/**
-                 * Everything defined as a child of Mode is only visible for users
-                 * in the UAC-Group 1
-                 *
-                 * The UAC-Group 1 is defined for chayns-Manager
-                 */}
-                <Mode mode={1} group={1}>
-                    <PersonFinder
-                        addUser={this.addUser}// Provide the addUser function to the person finder as a prop
-                    />
-                </Mode>
-                <UserList
-                    user={users} // Provide the user list array to the UserList element as a prop
-                    removeUser={this.removeUser}
-                />
+               
+                <Form />
+                <SiteList />
             </div>
         );
     }
---
+++ src/components/Utils/fetch.js
@@ -0,0 +1,13 @@
+	// ES8 getJson function
+	const getJson = async ($text, skip,take) => {
+		try{
+			let response = await fetch(`https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=${$text}&Skip=${skip}&Take=${take}`)
+			let json = await response.json();
+			return json.Data;
+		} catch (error) {
+			console.error(`readFile failed: ${error}`);
+		}	
+	}
+	
+	export default getJson;
+	
